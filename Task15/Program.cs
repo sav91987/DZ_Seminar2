452446d8b4a3503bc96668b98e255cf357d4a68a
Sotnikov_Andrey <sav91987@ya.ru>
2023-02-15 17:44:13 +0300

    Added Task15

---
+++ Task15/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введи цифру");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 0)
+{
+    number = number * (-1);
+}
+if ((number > 0) && (number <= 7))
+{
+    if ((number == 1) || (number == 2) || (number == 3) || (number == 4) || (number == 5))
+    {
+        Console.WriteLine("Будний день");
+    }
+    else
+    {
+        Console.WriteLine("Выходной день");
+    }
+}
+else
+{
+    Console.WriteLine("В неделе 7 дней. Введите корректное число");
+}
+
